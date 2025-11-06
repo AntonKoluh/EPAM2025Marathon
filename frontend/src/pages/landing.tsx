cf2ef29db6a79d78b52cc9a5bc4dcfd185210f64
Anton Koluh <antonkoluh1@gmail.com>
2025-11-06 12:11:51 +0200

    added some SVGs to landing page

--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router";
-import Button from "../components/buttons";
+import Button from "@/components/buttons";
+import fireworks from "@/assets/landing/firework.svg"
+import presents from "@/assets/landing/presents.svg"
+import present from "@/assets/landing/present.svg"
+import snowglobe from "@/assets/landing/snowglobe.svg"
+import raindeers from "@/assets/Raindeers.svg"
 
 export default function Landing(){
     return (
-        <div className="flex flex-col justify-start items-center gap-26 h-full">
-            <div>
+        <div className="relative flex flex-col justify-start items-center gap-26 h-full">
+            <div className="z-10">
     <h1 className="font-(family-name:--roboto-font) font-bold text-black text-xl sm:text-4xl text-center mt-25">Make This Holiday Magical <br/> with Seecret Nick</h1>
     <p className="font-semibold text-center text-xl mt-2">It's a secret - don't tell who you're matched with! <br/>Use the wishlist or preferences to pick the perfect gift. <br/> Be ready for the big gift exchange!</p>
     </div>
@@ -12,6 +17,13 @@ export default function Landing(){
     <Link to={"/create"} className="w-full text-center"><Button text={"Create New Room"} variant="main"/></Link>
     <Link to={"/join"} className="w-full text-center"><Button text={"Join Existing Room"} variant="secondary" /></Link>
     </div>
+        <img src = {fireworks} alt="fireworks" className="absolute left-3"/>
+        <img src = {presents} alt="presents" className="absolute right-2"/>
+        <img src = {raindeers} alt="raindeers" className="absolute right-1/2"/>
+        <img src = {present} alt="present" className="absolute left-6 bottom-8"/>
+        <img src = {snowglobe} alt="snowglobe" className="absolute right-10 bottom-8"/>
+        <img src = {snowglobe} alt="snowglobe" className="absolute right-3/4 top-65 z-0"/>
+        <img src = {fireworks} alt="fireworks" className="absolute left-3/4 top-65 z-0"/>
     </div>
 )
 }
